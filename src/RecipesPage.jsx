3eac45830db79ea1ffea68330ba4f253c9e308f6
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-07-15 16:21:54 +0200

    minor changes to RecipePage

--- src/RecipesPage.jsx
+++ src/RecipesPage.jsx
@@ -29,6 +29,7 @@ const RecipesPage = () => {
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
       <Header />
@@ -36,9 +37,10 @@ const RecipesPage = () => {
         <h1 className="text-4xl font-bold mt-10 mb-5">Recipes</h1>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {recipes.map((recipe) => (
-            <div key={recipe.id} className="w-64 h-64 bg-gray-200 rounded-md">
-              {recipe.title} {recipe.description} {recipe.ingredients}
-              {recipe.instructions}
+            <div key={recipe.id} className="w-64 h-64 bg-pink-300 rounded-md">
+              {recipe.title}
+              {/* {recipe.title} {recipe.ingredients}
+              {recipe.instructions} */}
             </div>
           ))}
         </div>
